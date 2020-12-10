17db22b5e117e2d073216653c8c3272e1200eaca
Данияр <danixan1019@outlook.com>
2020-12-10 20:16:17 +0400

    сегодня я создал страницу и покакал

---
+++ exam/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using exam.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace exam
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities Entity = new Entities();
+    }
+}
---
+++ exam/Views/ClientsList.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace exam.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsList.xaml
+    /// </summary>
+    public partial class ClientsList : Window
+    {
+        public ClientsList()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource clientViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientViewSource")));
+            // Загрузите данные, установив свойство CollectionViewSource.Source:
+            clientViewSource.Source = App.Entity.Client.ToList();
+        }
+    }
+}
